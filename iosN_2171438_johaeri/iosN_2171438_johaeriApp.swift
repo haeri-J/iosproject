d953466aaa370578944e280c299cbb2992ccc568
johaeri <harry123496@gmail.com>
2024-06-04 17:11:54 +0900

    Initial Commit

---
+++ iosN_2171438_johaeri/iosN_2171438_johaeriApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosN_2171438_johaeriApp.swift
+//  iosN_2171438_johaeri
+//
+//  Created by mac029 on 2024/06/04.
+//
+
+import SwiftUI
+
+@main
+struct iosN_2171438_johaeriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
