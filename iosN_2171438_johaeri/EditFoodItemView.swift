e5108607abbe29b28166078cb4fdca8a11773c49
johaeri <harry123496@gmail.com>
2024-06-14 14:29:09 +0900

    아이템 새로 등록시 사진 등록하면 바로 사진 보이도록 수정

--- iosN_2171438_johaeri/EditFoodItemView.swift
+++ iosN_2171438_johaeri/EditFoodItemView.swift
@@ -8,22 +8,27 @@
 import SwiftUI
 //사용자의 냉장고 재료 수정하기 위한 뷰
 struct EditFoodItemView: View {
-    @Binding var foodItem: FoodItem // FoodItem의 정보를 수정할 수 있도록 Binding 사용
+    @Binding var foodItem: FoodItem // 상위뷰의 FoodItem의 정보를 수정할 수 있도록 Binding 사용
     @Environment(\.presentationMode) var presentationMode // 뷰를 닫기 위한 환경 변수
     @State private var showingImagePicker = false //.sheet를 표시할지 여부를 정하는 변수
-    @State private var inputImage: UIImage?//사용자가 넣은 사진을 저장할 변수
+    @State private var inputImage: UIImage? {
+          didSet {
+              foodItem.image = inputImage //inputImage가 변경될 때마다 foodItem.image도 업데이트
+          }
+      }
+    
 
     var body: some View {
         Form {
             Section(header: Text("음식 정보")) {
-
+                
                 TextField("이름", text: $foodItem.name)//이름 받고 이름 넣기
                 DatePicker("유통기한", selection: $foodItem.expirationDate, displayedComponents: .date)//날짜받고 날자 저장
                 TextField("메모", text: $foodItem.memo)//메모 받고 메모저장
                 // 사진 추가 및 수정 기능 구현
                 Section {
-                    if foodItem.image != nil {//foodItem.image가 nil 아니면foodItem.image!을 보여줘라.
-                        Image(uiImage: foodItem.image!)
+                    if let image = inputImage {
+                        Image(uiImage: image)
                             .resizable()
                             .scaledToFit()
                     }
@@ -33,26 +38,15 @@ struct EditFoodItemView: View {
                 }
             }
             Button("완료") {
-                if let inputImage = inputImage {//inputImage가 있으면foodItem.image에 넣기
-                    foodItem.image = inputImage
-                } else {
-                    foodItem.image = foodItem.image//아니면 원래 있던 이미지 써라.
-                }
                 self.presentationMode.wrappedValue.dismiss() // 완료 버튼을 누르면 뷰를 닫음
             }
         }
         .navigationTitle("음식 정보 수정하기")
-        .sheet(isPresented: $showingImagePicker) {//$showingImagePicker가 트루면 ImagePicker를 열어서 $inputImage을 바인딩해라.
+        .sheet(isPresented: $showingImagePicker) {//$showingImagePicker가 트루면 ImagePicker 호출
             ImagePicker(selectedImage: $inputImage)
-              }
+        }
     }
 }
 
-struct EditFoodItemView_Previews: PreviewProvider {
-    @State static var foodItem = FoodItem(name: "1", expirationDate: Date(), memo: "아직은 신선하지만 언제까지 신선할지.", image: UIImage(named: "milk"))
 
-    static var previews: some View {
-        EditFoodItemView(foodItem: $foodItem)
-    }
-}
 
